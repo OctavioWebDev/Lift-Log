1e872483258c3eb8556c471479fd65028d8b3184
Octavio Sanchez <tavowebdev419@gmail.com>
2026-02-21 20:08:02 -0500

    Force dotenv override

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 console.log("========== SERVER STARTING ==========");
-import "dotenv/config";
+import dotenv from "dotenv";
+dotenv.config({ override: true });
 import express, { type Request, Response, NextFunction } from "express";
 import session from "express-session";  // ← Add this import
 import { registerRoutes } from "./routes";
